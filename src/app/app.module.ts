f61dec2e3fb5fa74315dc64ec74f6651aeef10f7
kesavan.muthusamy@syncfusion.com <kesavan.muthusamy@syncfusion.com>
2020-04-07 12:22:27 +0530

    area chart added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { ChartAllModule, AreaSeriesService } from '@syncfusion/ej2-angular-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartesianChartComponent } from './cartesian-chart/cartesian-chart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartesianChartComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ChartAllModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [CartesianChartComponent, AppComponent]
 })
 export class AppModule { }
